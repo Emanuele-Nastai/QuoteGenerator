d8a71889062110246fce3615367e9a774cbaf8eb
Emanuele-Nastai <91191665+Emanuele-Nastai@users.noreply.github.com>
2024-03-24 15:46:21 +0000

    Add styles

--- app/routes/SearchForm.tsx
+++ app/routes/SearchForm.tsx
@@ -1,6 +1,18 @@
 import React from "react";
+import {
+  TextInput,
+  ActionIcon,
+  useMantineTheme,
+  rem,
+  Center,
+  Box,
+  Flex,
+  getGradient,
+} from "@mantine/core";
+import { IconSearch, IconArrowRight } from "@tabler/icons-react";
 
 export default function SearchForm() {
+  const theme = useMantineTheme();
   const [quote, setQuote] = React.useState([] as any);
   async function sendRequest(event: any) {
     event.preventDefault();
@@ -21,24 +33,60 @@ export default function SearchForm() {
     }
   }
   return (
-    <div>
-      <form onSubmit={sendRequest}>
-        <input type="text" placeholder="Search..." />
-        <button type="submit">Search</button>
-      </form>
-      <div>
-        {quote && quote.length > 0 && (
-          <div>
-            <h2>{quote[0].quote}</h2>
-            <p>{quote[0].author}</p>
-          </div>
-        )}
-        {quote && quote.length == 0 && (
-          <div>
-            <h2>Topic not found.</h2>
-          </div>
-        )}
-      </div>
-    </div>
+    <Flex 
+      mih={50}
+      mt={350}  
+        gap="xl"
+        justify="center"
+        align="center"
+        direction="column"
+        wrap="wrap"
+      >
+      <Box>
+        <form onSubmit={sendRequest}>
+          <TextInput
+            radius="xl"
+            size="md"
+            placeholder="Search..."
+            rightSectionWidth={42}
+            leftSection={
+              <IconSearch
+                style={{ width: rem(18), height: rem(18) }}
+                stroke={1.5}
+              />
+            }
+            rightSection={
+              <ActionIcon
+                size={32}
+                radius="xl"
+                color={theme.primaryColor}
+                variant="filled"
+                type="submit"
+              >
+                <IconArrowRight
+                  style={{ width: rem(18), height: rem(18) }}
+                  stroke={1.5}
+                />
+              </ActionIcon>
+            }
+          />
+        </form>
+      </Box>
+      <Box>
+        <div>
+          {quote && quote.length > 0 && (
+            <div>
+              <h2>{quote[0].quote}</h2>
+              <p>{quote[0].author}</p>
+            </div>
+          )}
+          {!quote || quote.length == 0 && (
+            <div>
+              <h2>Topic not found.</h2>
+            </div>
+          )}
+        </div>
+      </Box>
+    </Flex>
   );
 }
